c887e334b75b7b39f4adc700d9ca6030cb3a2dc2
Seraj Elseen Abdullah <serageldeen13@gmail.com>
2024-12-19 16:32:55 +0200

    update

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -8,7 +8,7 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
-// import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from "react-responsive";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
@@ -17,18 +17,17 @@ const Ball = (props) => {
     <Float speed={1.75} rotationIntensity={1} floatingRange={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+      <mesh castShadow={false} receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 0]} />
         <meshStandardMaterial
+          color="#f5f5f5"
           emissive="#f5f5f5"
           emissiveIntensity={0.75}
-          polygonOffset
-          polygonOffsetFactor={-5}
           flatShading
         />
         <Decal
           position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
+          rotation={[Math.PI, 0, Math.PI / 2]}
           scale={1}
           map={decal}
           flatShading
@@ -39,15 +38,18 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
-  // const isSmallScreen = useMediaQuery({ maxWidth: 480 });
+  const isSmallScreen = useMediaQuery({ maxWidth: 480 });
 
   return (
     <Canvas
       frameloop="demand"
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
+      dpr={[1, isSmallScreen ? 1 : 2]}
+      gl={{ antialias: !isSmallScreen, preserveDrawingBuffer: false }}
     >
-      <OrbitControls enableZoom={false} enableRotate={false} />
+      <OrbitControls
+        enableZoom={!isSmallScreen}
+        enableRotate={!isSmallScreen}
+      />
       <Ball imgUrl={icon} />
       <Preload all />
     </Canvas>
